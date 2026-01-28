896dcf444b15493ff59d82441f831c96a0124a15
Tobias Richter <tobias.richter@diva-e.com>
2026-01-28 17:20:38 +0100

    Update src/logs.rs
    Co-authored-by: BerSomBen <benjamin.sommerfeld@diva-e.com>

--- src/logs.rs
+++ src/logs.rs
@@ -181,7 +181,7 @@ pub async fn tail_log(
                 }
                 // sum with current content length because we need a new range start value
                 // for our next request
-                last_content_length += last_content_length;
+                last_content_length += current_content_length;
                 sleep(Duration::from_secs(5));
             }
             StatusCode::RANGE_NOT_SATISFIABLE => {
